ff4f61faae6f5868b6a057a1ba2bc9bcebf73b1f
Will Shoemaker <shoemakah@gmail.com>
2015-10-25 13:38:30 -0500

    Fixed x-axis fig S9

--- Python/generate_figs_zipf.py
+++ Python/generate_figs_zipf.py
@@ -737,7 +737,6 @@ def zipf_mle_plots(data_dir= mydir):
         datasets = ['HMP', 'EMPclosed','EMPopen', 'MGRAST']
         #datasets = ['95','97','99']
         plot_dim = len(datasets)
-
         for i, dataset in enumerate(datasets):
             if  (dataset == 'EMPopen' or dataset == 'EMPclosed'):
                 zipf_data = import_NSR2_data(data_dir + 'NSR2/zipf_'+ dataset+'_NSR2.txt')
@@ -767,7 +766,6 @@ def zipf_mle_plots(data_dir= mydir):
             plt.ylim(np.amin(y),0)
             plt.xticks(fontsize = 6) # work on current fig
             plt.yticks(fontsize = 6)
-
             if dataset == 'HMP':
                 #ax.set_ylabel("HMP", rotation=90, size=12)
                 #plt.ylabel(r'$HMP$', rotation=90, fontsize = 12)
@@ -795,7 +793,7 @@ def zipf_mle_plots(data_dir= mydir):
             elif dataset == 'MGRAST':
                 ax.set_ylabel(r'MG-RAST' '\n' r'$\alpha$', rotation=90, size=12)
 
-            if i == 3:
+            if i == plot_dim-1:
                 plt.xlabel(r'$N_{0}$', fontsize = 12)
                 #ax.set_xlabel("N", rotation=0, size=12)
             predict_y = intercept + slope * N
@@ -824,7 +822,7 @@ def zipf_mle_plots(data_dir= mydir):
             plt.plot(x, predict_y, 'k-')
             plt.axhline(linewidth=2, color='darkgrey',ls='--')
 
-            if i == 3:
+            if i == plot_dim-1:
                 plt.xlabel(r"$N_{max}$", fontsize = 12)
             #plt.hline(0, xmin, xmax, color="0.3", ls='--')
             plt.ylabel(r'$\alpha$', fontsize = 12)
@@ -841,7 +839,7 @@ def zipf_mle_plots(data_dir= mydir):
 
             plt.xticks(fontsize = 6) # work on current fig
             plt.yticks(fontsize = 6)
-            if i == 2:
+            if i == plot_dim-1:
                 plt.xlabel(r'$\alpha$', fontsize = 12)
             plt.ylabel('Probability Density', fontsize = 7)
             #plt.legend(loc='best')
