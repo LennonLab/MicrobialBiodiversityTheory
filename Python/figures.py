99a320828e29aea4b87be9436686bd588842c9f2
Will Shoemaker <shoemakah@gmail.com>
2016-04-27 21:10:52 -0400

    made tables

--- Python/figures.py
+++ Python/figures.py
@@ -5,7 +5,7 @@ import macroecotools
 import numpy as np
 import  matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid.inset_locator import inset_axes
-import generate_figs_zipf as gfz
+#import generate_figs_zipf as gfz
 
 
 mydir = os.path.expanduser("~/github/MicroMETE/")
@@ -77,7 +77,7 @@ def Supp(figname = 'Supp', data_dir=mydir, radius=2):
     plt.savefig(fig_name, dpi=600)#, bbox_inches = 'tight')#, pad_inches=0)
     plt.close()
 
-def fig3(figname = 'Fig3', data_dir=mydir, radius=2):
+def figSuppp(figname = 'Fig3', data_dir=mydir, radius=2):
     fig = plt.figure()
     plot_dim = 2
     count = 0
@@ -129,6 +129,57 @@ def fig3(figname = 'Fig3', data_dir=mydir, radius=2):
         count += 1
     fig.text(0.50, 0.04, r'$Predicted$', ha='center', va='center')
     fig.text(0.05, 0.5, r'$Observed$', ha='center', va='center', rotation='vertical')
+    fig_name = str(mydir + 'figures/' + figname + '.png')
+    plt.savefig(fig_name, dpi=600)#, bbox_inches = 'tight')#, pad_inches=0)
+    plt.close()
+
+def fig3(figname = 'Fig3', data_dir=mydir, radius=2):
+    fig = plt.figure()
+    plot_dim = 1
+    count = 0
+
+    IN_Obs_Pred = importData.import_NSR2_data(mydir + \
+        'data/NSR2/Stratified/lognorm_pln_NSR2_stratify.txt')
+    N = np.asarray(list(((IN_Obs_Pred["N"]))))
+    S = np.asarray(list(((IN_Obs_Pred["S"]))))
+    NmaxObs = np.asarray(list(((IN_Obs_Pred["NmaxObs"]))))
+    NmaxPred = []
+    SPred = []
+    for i in range(len(N)):
+        NmaxPred_i = importPredictS.predictS(N[i], NmaxObs[i], predictNmax=True).getNmax()
+        SPred_i = importPredictS.predictS(N[i], NmaxObs[i], predictNmax=True).getS()
+        NmaxPred.append(NmaxPred_i)
+        SPred.append(SPred_i)
+    NmaxPred = np.asarray(NmaxPred)
+    SPred = np.asarray(SPred)
+    axis_min = 0
+    axis_max = 2 * max(NmaxPred)
+    #print plot_dim
+    ax = fig.add_subplot(plot_dim, plot_dim, count+1)
+    #ax.set_title(r"$\mathbf{N_{max}}$", y=1.03)
+
+    macroecotools.plot_color_by_pt_dens(NmaxPred, NmaxObs, radius, loglog=1,
+                    plot_obj=plt.subplot(plot_dim,plot_dim,count+1))
+    plt.plot([axis_min, axis_max],[axis_min, axis_max], 'k-')
+    plt.xlim(axis_min, axis_max)
+    plt.ylim(0, axis_max)
+    r2_all = macroecotools.obs_pred_rsquare(np.log10(NmaxObs), np.log10(NmaxPred))
+    r2text = r"${}^{{2}}_{{m}} = {:.{p}f} $".format('r',r2_all , p=2)
+    plt.text(0.18, 0.93, r2text,  fontsize=14,
+        horizontalalignment='center',
+        verticalalignment='center',transform = ax.transAxes)
+    plt.tick_params(axis='both', which='major', labelsize=7)
+    plt.subplots_adjust(wspace=0.5, hspace=0.3)
+
+    #axins = inset_axes(ax, width="30%", height="30%", loc=4)
+
+    ax.set(adjustable='box-forced', aspect='equal')
+    #plt.setp(axins, xticks=[], yticks=[])
+
+    count += 1
+    fig.text(0.50, 0.04, r'$Predicted\, log_{10}(N_{max})$', ha='center', va='center', fontsize = 16)
+    fig.text(0.2, 0.5, r'$Observed\,log_{10}(N_{max})$', ha='center', va='center', rotation='vertical',\
+        fontsize = 16)
     fig_name = str(mydir + 'figures/' + figname + '.png')
     plt.savefig(fig_name, dpi=600)#, bbox_inches = 'tight')#, pad_inches=0)
     plt.close()
